7b055c2af1a60b4bfea97094f437367ba48317bc
Manuel Viqueira Rodríguez <joaodevigo@gmail.com>
2022-05-08 22:51:22 +0200

    Cleanup

--- Synthesization/Run.sh
+++
@@ -1,7 +0,0 @@
-export Reports_DIR="logfiles"
-
-mkdir $Reports_DIR
-
-cd work
-
-dc_shell -f synthesis.tcl | tee -i $Reports_DIR/synthesis.log
--- Synthesization_CORDIC/Run_CORDIC.sh
+++
@@ -1,7 +0,0 @@
-export Reports_DIR="logfiles"
-
-mkdir $Reports_DIR
-
-cd work
-
-dc_shell -f synthesis_CORDIC.tcl | tee -i $Reports_DIR/synthesis_CORDIC.log
